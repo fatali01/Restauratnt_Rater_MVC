5756ac6b863dfd34f9ca3dcd548b90d86d9f8c07
Fatali Fataliyev <fataliyevfatali2001@gmail.com>
2024-02-06 17:54:08 -0500

    main

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestauratntRaterMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
